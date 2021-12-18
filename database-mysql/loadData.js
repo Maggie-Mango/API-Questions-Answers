a9b266b5038f807b8ffe0406ecdcbca93ace1df7
Maggie-Mango <maggiesaldivia@gmail.com>
2021-12-18 09:27:57 -0800

    add joins and proper date syntax

--- database-mysql/loadData.js
+++ database-mysql/loadData.js
@@ -12,72 +12,80 @@ connection.connect((err) => {
     console.log(err);
   }
   console.log('connected to sql database')
-});
-
-function loadQuestions() {
-  return new Promise((resolve, reject) => {
-  connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE questions
-  FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)`,
-  [pathToQuestions],
-  function(err, results) {
-    if (err) {
-      console.log(err);
-    } else {
-      resolve(console.log('insert data into QUESTIONS complete'));
+  function loadQuestions() {
+    return new Promise((resolve, reject) => {
+    console.log('loading data into Questions')
+    connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE questions
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)`,
+    [pathToQuestions],
+    function(err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(console.log('insert data into QUESTIONS complete'));
+      }
     }
+  )
+    })
   }
-)
-  })
-}
 
-function loadAnswers() {
-  return new Promise((resolve, reject) => {
-  connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE answers
-  FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)`,
-  [pathToAnswers],
-  function(err, results) {
-    if (err) {
-      console.log(err);
-    } else {
-      resolve(console.log('insert data into ANSWERS complete'));
+  function loadAnswers() {
+    return new Promise((resolve, reject) => {
+    console.log('loading data into Answers')
+    connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE answers
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)`,
+    [pathToAnswers],
+    function(err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(console.log('insert data into ANSWERS complete'));
+      }
     }
+  )
+  })
   }
-)
-})
-}
 
-function loadAnswersPhotos() {
-  return new Promise((resolve, reject) => {
-  connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE answers_photo
-  FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (id, answer_id, url)`,
-  [pathToAnswersPhotos],
-  function(err, results) {
-    if (err) {
-      console.log(err);
-    } else {
-      resolve(console.log('insert data into ANSWER PHOTOS complete'))
-    }
-  })
-  })
-}
-const promise1 = loadQuestions()
-const promise2 = loadAnswers()
-const promise3 = loadAnswersPhotos()
+  function loadAnswersPhotos() {
+    return new Promise((resolve, reject) => {
+    console.log('loading data into Photos')
+    connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE answers_photo
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (id, answer_id, url)`,
+    [pathToAnswersPhotos],
+    function(err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(console.log('insert data into ANSWER PHOTOS complete'))
+      }
+    })
+    })
+  }
+
+  const promise1 = loadQuestions()
+  const promise2 = loadAnswers()
+  const promise3 = loadAnswersPhotos()
 
 
 Promise.all([promise1, promise2, promise3]).then(function(values) {
   console.log('all complete now');
-  console.log('joining tables now...')
-  connection.query(`
-
-  `)
+  connection.end()
 });
+
+});
+
+
+
+
+
+
+
--- schemas/schema.sql
+++ schemas/schema.sql
@@ -2,13 +2,14 @@ DROP DATABASE IF EXISTS days_a_week;
 
 CREATE DATABASE days_a_week;
 
+
 USE days_a_week;
 
 CREATE TABLE questions (
   id INT NOT NULL AUTO_INCREMENT,
   product_id INT,
   body VARCHAR(600),
-  date_written VARCHAR(100),
+  date_written bigint NOT NULL,
   asker_name varchar(100),
   asker_email VARCHAR(100) NOT NULL,
   reported INT,
@@ -20,7 +21,7 @@ CREATE TABLE answers (
   id INT NOT NULL AUTO_INCREMENT,
   question_id INT,
   body VARCHAR(600) NOT NULL,
-  date_written VARCHAR(100),
+  date_written bigint NOT NULL,
   answerer_name VARCHAR(100) NOT NULL,
   answerer_email VARCHAR(100) NOT NULL,
   reported INT,
@@ -34,3 +35,28 @@ CREATE TABLE answers_photo (
   url VARCHAR(200),
   PRIMARY KEY (id)
 );
+
+SELECT
+  q.product_id,
+  q.body,
+  q.FROM_UNIXTIME(a.date_written/1000) as date_time,
+  q.answerer_name,
+  q.answerer_email,
+  q.reported,
+  q.helpful
+FROM questions q
+JOIN (SELECT  a.id,
+        a.question_id,
+        a.body as answer_body,
+        FROM_UNIXTIME(q.date_written/1000) as date_time,
+        a.answerer_name,
+        a.answerer_email,
+        a.reported,
+        a.helpful,
+        ap.id as ap_id,
+        ap.url
+FROM answers a
+JOIN answers_photo ap
+ON a.id = ap.answer_id) as answers
+ON questions.id = answers.id
+LIMIT 30/K;
